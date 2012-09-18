b138653681c41be29e21f5995c7355bb89d79daa
Valeriy Van <val@w7software.com>
2012-09-18 18:07:33 +0300

    Initial Commit

---
+++ TextWatermark/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TextWatermark
+//
+//  Created by Valeriy Van on 18.09.12.
+//  Copyright (c) 2012 w7software.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
